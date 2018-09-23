87b28a2f8d73bf3dffa528e62330c33d6b0f8bfc
Manpvl <010375@list.ru>
2018-09-23 17:26:22 +0300

    В main.js, создали const menu с функцией и применили ее в index.html. В general.css поставили для img box-shadow, сократили ”red-asterix”. В index.html вставили вызов функции в меню. В zooschop.css для table поставили box-shadow и тега iframe margin-top.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const siteNames = {
     "index":`<i>Зоомагазин - Питомец</i>`,
     "about":`<i>Зоомагазин - О компании</i>`,
     "articl":`<i>Зоомагазин - Статьи</i>`,
-    "to_order":`<i>Зоомагазин - Заказ</i>`,
+    "to_order":`<i>Зоомагазин - Заказ<br/>+7-905-550-45-45 </i>`,
     "blog":`<i>Мой Блог</i>`,
     "default":`<i>Зоомагазин - Питомец</i>`,
 };
@@ -21,4 +21,18 @@ function getSiteName() {
     //let siteName = `<i class="name-one">Зоомагазин</i>`;
     //console.log("Выводим название сайта" + siteName);
     //return siteName;
+}
+const menu =   `<a href="index.html">Главная</a> |
+                <a href="about.html">О компании</a> |
+                <a href="articl.html">Статьи</a> |
+                <a href="breed.html">Порода</a> |
+                <a href="rare.html">Редкие</a> |
+                <a href="care.html">Уход</a> |
+                <a href="competition.html">Конкурс</a> |
+                <a href="to_order.html">Заказ товара</a> |
+                <a href="contacts.html">Контакты</a> |
+                <a href="blog.html">Blog</a> |`
+
+function getMenu() {
+    return menu
 }
